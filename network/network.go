f4ad19a60d88f0117358a7bc072979d641972e4e
Bernhard Paus Græsdal <felgen@klypa.ntnu.no>
2019-03-20 18:02:33 +0100

    Implement peerlist support in network module

--- network/network.go
+++ network/network.go
@@ -4,6 +4,8 @@ import (
 	"time"
 	"fmt"
 	"./driver/bcast"
+	"./driver/peers"
+	"strconv"
 	"../fsm"
 )
 
@@ -25,16 +27,33 @@ func Module(
 	LocalNodeStateChan <-chan fsm.NodeState) {
 
 	// Setup channels and modules for sending and receiving localStateMsg
+	// -----
 	localStateTx := make(chan localStateMsg)
 	localStateRx := make(chan localStateMsg)
 	go bcast.Transmitter(16569, localStateTx)
 	go bcast.Receiver(16569, localStateRx)
 
-	
+	// Configure Peer List
+	// -----
+	peerUpdateChan := make(chan peers.PeerUpdate)
+	peerTxEnable := make(chan bool) // Used to signal that the node is unavailable
+	go peers.Transmitter(15647, strconv.Itoa(int(localID)), peerTxEnable)
+	go peers.Receiver(15647, peerUpdateChan)
+
+
+	// Handle network traffic
+	// -----
+
 	localState := fsm.NodeState {}
 
 	for {
 		select {
+
+		case a := <-peerUpdateChan:
+			fmt.Printf("Peer update:\n")
+			fmt.Printf("  Peers:    %q\n", a.Peers)
+			fmt.Printf("  New:      %q\n", a.New)
+			fmt.Printf("  Lost:     %q\n", a.Lost)
 
 		case a := <-LocalNodeStateChan:
 			localState = a
@@ -43,11 +62,12 @@ func Module(
 				State: localState,
 			}
 
-		case a := <- localStateRx:
+		/*case a := <- localStateRx:
 			if a.ID != localID {
 				fmt.Println("(network) Received from id: ", a.ID)
 				fmt.Println("   State: ", a.State)
-			}
+			}*/
+
 		}
 	}
 }
